e42d63cd29209c9aac565ac5b01844c240041152
lalitsaraswa2 <lalitsaraswat418@gmail.com>
2025-05-08 23:17:58 +0530

    crm_prontend

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-      // repo name
-      base:"crm_frontend",
+      // repository name
+      base:"/crm_frontend/",
       
   plugins: [react(),
     tailwindcss(),
